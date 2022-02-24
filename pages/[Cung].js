914ea50de847569bd5fbd4bd143100ca293fed17
ndnghia <ndnghiadn@gmail.com>
2022-02-24 15:07:58 +0700

    Fix api route

--- pages/[Cung].js
+++ pages/[Cung].js
@@ -134,7 +134,7 @@ export default function CungComponent({ data }) {
 
 export async function getServerSideProps(context) {
     const { Cung } = context.query
-    const res = await fetch(`http://localhost:3000/api/Cung/${Cung}`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/Cung/${Cung}`)
     const data = await res.json()
     return {
         props: {
--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home({ data }) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`http://localhost:3000/api/Cung`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/Cung`)
   const data = await res.json()
   return {
       props: {
