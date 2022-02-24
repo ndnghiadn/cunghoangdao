9b9fcd9b1873b4782a07b576f37473a363d3c3ef
ndnghia <ndnghiadn@gmail.com>
2022-02-24 07:20:10 +0700

    first init

---
+++ pages/[Cung].js
@@ -0,0 +1,145 @@
+import Head from 'next/head'
+import Router from 'next/router'
+import { useEffect, useState } from 'react'
+import Page404 from './404.js'
+import styles from '../styles/CungComponent.module.css'
+import { Collapse } from 'antd'
+const { Panel } = Collapse
+import { ArrowLeftOutlined } from '@ant-design/icons'
+import Particles from 'react-tsparticles'
+
+import * as DATA from '../data.json'
+
+export default function CungComponent({ Cung }) {
+    const [targetCung, setTargetCung] = useState()
+
+    useEffect(() => {
+        setTargetCung(DATA.find(item => item.query === `/${Cung}`))
+        console.log('targetCung: ', DATA.find(item => item.query === `/${Cung}`))
+    }, [])
+
+    return targetCung ? (
+        <div className={styles.container}>
+            <Head>
+                <title>Cung Hoàng Đạo - {targetCung.name}</title>
+            </Head>
+            <Particles
+                className={styles.background}
+                options={{
+                    background: {
+                      color: {
+                        value: "#232741",
+                      },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                      events: {
+                        onClick: {
+                          enable: true,
+                          mode: "push",
+                        },
+                        onHover: {
+                          enable: true,
+                          mode: "grab",
+                        },
+                        resize: true,
+                      },
+                      modes: {
+                        bubble: {
+                          distance: 250,
+                          duration: 2,
+                          opacity: 0,
+                          size: 0,
+                        },
+                        push: {
+                          quantity: 4,
+                        },
+                        repulse: {
+                          distance: 400,
+                        },
+                      },
+                    },
+                    particles: {
+                      color: {
+                        value: "#ffffff",
+                      },
+                      links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                      },
+                      collisions: {
+                        enable: true,
+                      },
+                      move: {
+                        direction: "none",
+                        enable: true,
+                        outMode: "bounce",
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                      },
+                      number: {
+                        density: {
+                          enable: true,
+                          area: 400,
+                        },
+                        value: 20,
+                      },
+                      opacity: {
+                        value: 0.5,
+                      },
+                      shape: {
+                        type: "circle",
+                      },
+                      size: {
+                        random: true,
+                        value: 5,
+                      },
+                    },
+                    detectRetina: true,
+                  }}
+            />
+            <div>
+                <ArrowLeftOutlined className={styles.arrow} onClick={() => Router.back()}/>
+                <h1 className={styles.name}>{targetCung.name}</h1>
+            </div>
+            <img src={targetCung.image} alt={targetCung.name} className={styles.image}/>
+            <Collapse
+                className={styles.collapse}
+                defaultActiveKey="1"
+                expandIconPosition={'left'}
+            >
+                <Panel header="Tính cách" key="1">
+                    { targetCung.personality?.map((paragraph, index) => <div key={index} className={styles.paragraph}>{paragraph}</div>) }
+                </Panel>
+                <Panel header="Tình cảm" key="2">
+                    { targetCung.love?.map((paragraph, index) => <div key={index} className={styles.paragraph}>{paragraph}</div>) }
+                </Panel>
+                <Panel header="Gia đình" key="3">
+                    { targetCung.family?.map((paragraph, index) => <div key={index} className={styles.paragraph}>{paragraph}</div>) }
+                </Panel>
+                <Panel header="Sự nghiệp" key="4">
+                    { targetCung.career?.map((paragraph, index) => <div key={index} className={styles.paragraph}>{paragraph}</div>) }
+                </Panel>
+                <Panel header="Tài chính" key="5">
+                    { targetCung.finance?.map((paragraph, index) => <div key={index} className={styles.paragraph}>{paragraph}</div>) }
+                </Panel>
+                <Panel header="Fun fact" key="6">
+                    { targetCung.fact?.map((paragraph, index) => <div key={index} className={styles.paragraph}>{paragraph}</div>) }
+                </Panel>
+            </Collapse>
+        </div>
+    ) : <Page404 />
+}
+
+export async function getServerSideProps(context) {
+    const { Cung } = context.query
+    return {
+        props: {
+            Cung
+        },
+    }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import 'antd/dist/antd.css'
+import Router from 'next/router'
+import Head from 'next/head'
+import NProgress from 'nprogress'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+NProgress.configure({ showSpinner: false })
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Import CSS for nprogress */}
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp
